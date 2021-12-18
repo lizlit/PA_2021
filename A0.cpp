7339fb01f2e1e6fda52da16054324e5ca849b801
Елизавета Литвинова <e.litvinova@goodcustomers.ru>
2021-12-19 02:49:31 +0700

    A0 is corrected

--- A0.cpp
+++ A0.cpp
@@ -1,28 +1,28 @@
 #include <iostream>
+#include <cstring>
+
 using namespace std;
-int main()
+int main(int argc, char* argv[])
 {
-  char s[8000];
+  
+  int n = strlen(argv[1]);
   int word, count = 0;
-  //system("chcp 1251");
-  //system("cls");
-  cout << "Enter the string: " << endl;
-  cin.get(s, 8000);
   int i = 0;
-  while (s[i] == ' ' && s[i] != '\0')
+  while (argv[1][i] == ' ' && argv[1][i] != '\0')
     i++;
   word = 0;
-  while (s[i] != '\0') {
-    if ((s[i] != ' ' || s[i] == '\t' || s[i] == '\n') && word == 0)
+  while (argv[1][i] != '\0') {
+    if ((argv[1][i] != ' ' || argv[1][i] == '\t' || argv[1][i] == '\n') && word == 0)
     {
       word = 1;
       count++;
     }
-    else if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' )
+    else if (argv[1][i] == ' ' || argv[1][i] == '\t' || argv[1][i] == '\n' )
       word = 0;
     i++;
   }
-  cout << "Number of words in the string " << count;
-  cin.get(); cin.get();
+  cout << "Number of words in the string " << count << endl;
+
   return 0;
 }
+
